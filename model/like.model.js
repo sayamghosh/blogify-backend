5119ebe95444d435e64fbfdeb28c536632b9b120
Sayam Ghosh <sayamghosh.dev@gmail.com>
2025-04-14 08:01:00 +0530

    like model added

---
+++ model/like.model.js
@@ -0,0 +1,21 @@
+// models/like.model.ts
+const {Schema,model} = require('mongoose')
+
+const likeSchema = new Schema({
+  blogId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Blog',
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+}, { timestamps: true });
+
+likeSchema.index({ blogId: 1, userId: 1 }, { unique: true }); // Prevent duplicate likes
+
+const Like = model('Like', likeSchema);
+
+module.exports = Like;
